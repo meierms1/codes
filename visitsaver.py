6c50b064f5df71219b3c06a972e9b21abefe77ea
Maycon Meier <mmeierdo@uccs.edu>
2022-09-06 14:18:28 -0600

    up

--- visitsaver.py
+++ visitsaver.py
@@ -5,11 +5,33 @@ drt = "countourCurves"
 prt = "/home/mmeierdo/solidphase/outs/output/"
 path = os.path.join(prt,drt)
 os.mkdir(path)
- 
+
+a={}
+with open(prt + "metadata") as f:
+    for line in f: 
+        if line[0] != "#" and line[0] != "\n" and line[0] != " ":
+            #print(line)
+            k,v = line.split(" = ")
+            v = v.split(" #")[0]
+            a[k] = v
+special = ["geometry.prob_lo", "geometry.prob_hi", "amr.n_cell", "phi.ic.laminate.center"]
+for i in a.keys():
+    a[i] = a[i].replace("\n", "")
+    if " " in a[i]:
+        if i not in special:
+            a[i] = a[i].replace(" ", '')
+
+steps = 50
+if a["Status"][0] == "C":
+    steps = float(a["stop_time"]) / float(a["amr.plot_dt"])
+else:
+    g = ""
+    for i in a["Status"]:
+        if i <= '9' and i >= '0'"
+            g += i 
+    steps = float(g) / 100 * float(a["stop_time"]) / float(a["amr.plot_dt"])
 
 OpenDatabase("localhost:/home/mmeierdo/solidphase/outs/output/celloutput.visit", 0)
-
-size = 200
 
 AddPlot("Contour", "eta", 1, 1)
 ContourAtts = ContourAttributes()
@@ -26,9 +48,7 @@ ContourAtts.wireframe = 0
 SetPlotOptions(ContourAtts)
 DrawPlots()
 
-for i in range(size):
-    ContourAtts = ContourAttributes()
-    ContourAtts.contourNLevels = 1
+for i in range(steps):
     SaveWindowAtts = SaveWindowAttributes()
     SaveWindowAtts.outputToCurrentDirectory = 0
     SaveWindowAtts.outputDirectory = "/home/mmeierdo/solidphase/outs/output"
@@ -51,5 +71,7 @@ for i in range(size):
     SetSaveWindowAttributes(SaveWindowAtts)
     SaveWindow()
     TimeSliderNextState()
+
+
 
 
