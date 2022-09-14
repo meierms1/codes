391b07fe85d0fde523ed1c716d21a763fd3eb389
maycon <meierms@icloud.com>
2022-09-14 16:23:41 -0600

    daily commit

--- CurveCall.py
+++ CurveCall.py
@@ -8,9 +8,11 @@ Created on Fri Sep  9 10:20:52 2022
 import numpy as np
 import matplotlib.pyplot as plt
 from CURVE import curve, outliers, reader
+import getpass, time
 
-var = ["outputap100","outputap200", "outputap300"] #, "outputmix400", "output500"] 
-names = ["100","200", "300"] #, "400", "500"] 
+var = ["output05","output20", "output40", "output60"]
+var = ["outputap400", "outputap500"] 
+names = ["P=0.5MPa","P=2.0MPa", "P=4.0MPa", "P=6.0MPa"] #, "400", "500"] 
 st = [100,100,100]# ,100,100]
 dt = [0.1,0.1,0.1]# ,0.1,0.1]
 #var = ["outputap"]
@@ -19,31 +21,41 @@ dt = [0.1,0.1,0.1]# ,0.1,0.1]
 xsize = 10
 ysize = 8
 bar = 4
-inlab = True
+if getpass.getuser() == "meierms":
+    inlab = False
+else:
+    inlab = True
 
 j = 0
 fig1, ax = plt.subplots(figsize=(xsize,ysize))
 fig2, bx = plt.subplots(figsize=(xsize,ysize))
 fig3, cx = plt.subplots(figsize=(xsize,ysize))
 
+h = time.localtime()
+name = "/home/meierms/solidphase/outs/Record_" + str(h[2]) + "_" + str(h[3]) + "_" + str(h[4])
+
 for i in var: 
     print(i)
     if not inlab:
-        time, point, speed, c = curve(i)
+        time_, point, speed, c = curve(i)
     else: 
-        time, point, speed, c = reader(i, st[j] , dt[j])
+        time_, point, speed, c = reader(i, st[j] , dt[j])
     zero_mean = np.mean(speed)
     nspeed = outliers(speed, bar=bar)
     nmean = np.mean(nspeed)
+    z = int(len(speed) / 2)
+    hmean = np.mean(nspeed[z:])
 
-    print(names[j] +": --No-Filter: " + str(zero_mean) + " -- Filtered: "  + str(nmean))
+    print(names[j] +": --No-Filter: " + str(zero_mean) + " --Filtered: "  + str(nmean) + " --Hon: " + str(hmean))
     
-    ax.plot(time, point, label = names[j])
-    bx.plot(time[:-1], speed, label = names[j])
-    cx.plot(time[:-1], nspeed, label = names[j])
+    ax.plot(time_, point, label = names[j])
+    bx.plot(time_[:-1], speed, label = names[j])
+    cx.plot(time_[:-1], nspeed, label = names[j])
     
     j += 1
-    
+
+    with open(name, "a") as namei: np.savetxt(namei,nspeed); namei.write("###########\n" ) ; np.savetxt(namei,[zero_mean, nmean, hmean] ) ; namei.write("####!!###!!####\n")
+
  
 ax.set_title("Interface Location")
 bx.set_title("Burn Rate")
